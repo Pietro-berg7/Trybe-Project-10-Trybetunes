9340bf61859d2f8dcdc5d3365e3cc50be20c8d54
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-10-13 05:26:17 -0300

    Requisito 1 concluído

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Favorites from './pages/Favorites';
+import Album from './pages/Album';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/album/:id" component={ Album } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/search" component={ Search } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
